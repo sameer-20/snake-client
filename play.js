a5cbc07513fa3eb441ea2dce747a2eee0470592f
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-07-29 19:02:00 +0000

    Refactored code

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 //
 
-const net = require ('net');
-
-// Establishes connection with the game server
-const connect = function () {
-  const conn = net.createConnection ({
-    host: '135.23.222.131',
-    port: 50541
-  });
-  conn.setEncoding('utf8'); // Interpret incoming data as text
-  
-  conn.on('connect', () => {  
-    conn.write("Name: S@m"); // Adding name to the snake
-  })
-
-  conn.on('data', (message) => {  // Logging message from the server
-    console.log(message);
-  }) 
-
-  return conn;
-};
-
+const connect = require ('./client');
 
 console.log('Connecting ...');
 connect();
